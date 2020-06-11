86fb1090bb5735e5164b52485fed234b91a7ce77
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-11 17:04:57 +1000

    Initial check-in of module CSharpBasicsWithMosh

---
+++ CSharpBasicsWithMosh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasicsWithMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
