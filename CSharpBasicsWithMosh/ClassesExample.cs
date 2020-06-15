5d115c282afade1d48baedabf8c8678a6d10a3e6
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-15 13:55:39 +1000

    Classes

---
+++ CSharpBasicsWithMosh/ClassesExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpBasicsWithMosh
+{
+    public class ClassesExample
+    {
+        public string ReturnString()
+        {
+            return "I am a string called from within the ClassesExample class from the ReturnString method";
+        }
+    }
+}
+
+/* Classes are the building blocks of Object Orientated programming, 
+ * that combine the related variables(fields) with Functions(methods).
+ * 
+ * Class    - Person
+ * 
+ * Fields   - Name: String
+ *          - Age: bite
+ *          - Height: Float
+ *          - Weight: Int
+ *          
+ * Methods  - Walk()
+ *          - Talk()
+ *          - Eat()
+ *          - Sleep()
+*/ 
--- CSharpBasicsWithMosh/Program.cs
+++ CSharpBasicsWithMosh/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpBasicsWithMosh
 {
@@ -6,7 +7,8 @@ namespace CSharpBasicsWithMosh
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
         }
     }
 }
