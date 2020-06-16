6176711a6369dec5cc63f83a992c8906513a1de5
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-16 15:17:47 +1000

    Enums

---
+++ CSharpBasicsWithMosh/Enum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpBasicsWithMosh
+{
+    public enum Season
+    {
+        Spring = 1,
+        Summer = 2,
+        Autumn = 3,
+        Winter = 4
+    }
+
+    public class EnumMethods
+    {
+        public static void TestEnum()
+        {
+            // Grabbing the enum value
+            var grabEnum = Season.Winter; // Assign the enum
+            // Use 'Casting' to convert the enum to an int
+            Console.WriteLine((int)grabEnum); // output => 4
+
+            // Recalling an enum by number
+            int enumNumber = 3;
+            // Cast the enum and pass in the number location required
+            Console.WriteLine((Season)enumNumber); // outcome => "Autumn"
+
+            // Parse (changing data type) to an enum
+            string convertSeason = "Spring";
+            // convertedEnum is (casting) the Enum.Parse method and grabbing the typeof Season(ie and enum)
+            // and passing in the string to convert.
+            var convertedEnum = (Season) Enum.Parse(typeof (Season), convertSeason);
+            Console.WriteLine(convertedEnum);
+        }
+    }
+    
+}
+
+/* Enumeration is a value type defined by a set of named constants 
+ * of the underlying integral numeric types (i.e bite, short, int, long).
+ * 
+ * By default an Enum are of type 'int' if not defined.
+ * 
+*/
