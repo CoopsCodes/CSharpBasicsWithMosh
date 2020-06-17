ab7bfdd753b0cc6e4d0eba30bf933f213de481cb
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-17 12:53:46 +1000

    Conditionals: If Statement

---
+++ CSharpBasicsWithMosh/ConditionalStatements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CSharpBasicsWithMosh
+{
+    public class ConditionalStatements
+    {
+        public void IfStatement()
+        {
+            // If Statements check if a condition is true and process it
+
+            // The statement being checked
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It is morning");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It is Afternoon");
+            }
+            else
+            {
+                Console.WriteLine("It is Night");
+            }
+        }
+    }
+}
