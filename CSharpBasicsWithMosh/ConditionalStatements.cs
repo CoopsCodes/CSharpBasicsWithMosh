0d8ba72b73505d5de50df0a2f4a404a4116ebb0b
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-17 13:26:34 +1000

    Conditionals: Conditional Operator

--- CSharpBasicsWithMosh/ConditionalStatements.cs
+++ CSharpBasicsWithMosh/ConditionalStatements.cs
@@ -23,5 +23,27 @@ namespace CSharpBasicsWithMosh
                 Console.WriteLine("It is Night");
             }
         }
+
+        public void ConditionalOperator()
+        {
+            // A shorter hand form of an if statement written in a single line.
+            // This example uses the if statement without the curly braces,
+            // can ONLY be used if the code within the if statemend is only one line.
+
+            // Tradition basic if statement.
+            bool isGoldCustomer = true;
+
+            float price;
+
+            if (isGoldCustomer)
+                price = 19.95f;
+            else
+                price = 29.95f;
+
+            // Can be rewitten as a Conditional Operator
+            // Yes it is almost identical to a Ternary Operator in Javascript
+            float conditionalPrice = (isGoldCustomer) ? 19.95f : 29.95f; 
+        }
+
     }
 }
--- CSharpBasicsWithMosh/Program.cs
+++ CSharpBasicsWithMosh/Program.cs
@@ -8,7 +8,7 @@ namespace CSharpBasicsWithMosh
         static void Main(string[] args)
         {
             ClassesExample test = new ClassesExample(); // Invoking an object of the ClassesExample.
-            //var test = new ClassesExample(); => can also be defined with the var keyword
+            //var test = new ClassesExample(); => can also be defined with the Var declaration
             test.Name = "Cooper"; // Assigning a string to the Name variable decalred in the ClassesExamples Class.
             test.TestString(); // Calling the method within the class => ""Oh hey, my name is Cooper"
         }
