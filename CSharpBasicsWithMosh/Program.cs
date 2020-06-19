fe351760daabd41273a8ba644557037add208763
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-19 13:12:23 +1000

    Random Number generation

--- CSharpBasicsWithMosh/Program.cs
+++ CSharpBasicsWithMosh/Program.cs
@@ -12,6 +12,19 @@ namespace CSharpBasicsWithMosh
             test.Name = "Cooper"; // Assigning a string to the Name variable decalred in the ClassesExamples Class.
             test.TestString(); // Calling the method within the class => ""Oh hey, my name is Cooper"
         }
+
+        static void Random()
+        {
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(random.Next()); // Next returns a non negative integer
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(random.Next(1, 10));  // Next can take a min and max param,
+                                                        // this will only generate a number between 1 and 10
+
+
+        }
     }
 }
 
