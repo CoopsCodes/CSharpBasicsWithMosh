ff141cb57dfdd144716930e0f7fb79226457ce76
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-19 13:29:14 +1000

    Random Password function

--- CSharpBasicsWithMosh/Program.cs
+++ CSharpBasicsWithMosh/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CSharpBasicsWithMosh
 {
@@ -16,14 +15,29 @@ namespace CSharpBasicsWithMosh
         static void Random()
         {
             var random = new Random();
+
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(random.Next()); // Next returns a non negative integer
 
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(random.Next(1, 10));  // Next can take a min and max param,
                                                         // this will only generate a number between 1 and 10
+        }
 
+        static void RandomPassword()
+        {
+            var random = new Random();
+            int passwordLength = 10;
+            char[] buffer = new char[passwordLength];
 
+            for (int i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            string password = new string(buffer);
+
+            Console.WriteLine(password);
         }
     }
 }
